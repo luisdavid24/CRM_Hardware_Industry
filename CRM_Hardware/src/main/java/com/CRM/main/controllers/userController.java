9a52edfae76b621b5edebbb98eef85cb0199e286
Julian362 <Juliangarcia362@hotmail.com>
2022-10-03 21:22:58 -0500

    add model and controller for users

---
+++ CRM_Hardware/src/main/java/com/CRM/main/controllers/userController.java
@@ -0,0 +1,5 @@
+package com.CRM.main.controllers;
+
+public class userController {
+    
+}
---
+++ CRM_Hardware/src/main/java/com/CRM/main/model/User.java
@@ -0,0 +1,36 @@
+package com.CRM.main.model;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table (name = "users")
+@ToString
+public class User {
+        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Getter @Setter
+    private int id;
+    
+    @Column(name = "name", nullable = true)
+    @Getter @Setter
+    private String name;
+    
+
+    @Column(name = "email", nullable = false)
+    @Getter @Setter
+    private String email;
+    
+    @Column(name = "phone", nullable = false)
+    @Getter @Setter
+    private String phone;
+    
+    @Column(name = "password", nullable = false)
+    @Getter @Setter 
+    private String password;
+    
+}
