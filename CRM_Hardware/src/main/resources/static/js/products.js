1eab20e06d82e1abf8e1d85537f01c29c851d37a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-05 12:23:50 -0500

    feat: Add insert product and modal form

--- CRM_Hardware/src/main/resources/static/js/products.js
+++ CRM_Hardware/src/main/resources/static/js/products.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
 let productToModify;
 
+
 async function loadProducts() {
 
     const request = await fetch('api/products', {
@@ -13,40 +14,39 @@ async function loadProducts() {
             'Content-Type': 'application/json',
             'Authorization': localStorage.token
         }
-        //body: JSON.stringify({a: 1, b: 'Textual content'})
     });
     const productsHTML = await request.json();
     console.log(productsHTML);
-  
+
     let listHTML = '';
-    
-    for(let it_product of productsHTML){
-        btnDelete = "<button onclick=deleteProduct(\'"+it_product.productCode+"\')>\n\
+
+    for (let it_product of productsHTML) {
+        btnDelete = "<button onclick=deleteProduct(\'" + it_product.productCode + "\')>\n\
                     <i class='bi bi-trash'></i>\n\
                     </button>";
-        btnEdit = "<button onclick=loadData(\'"+it_product.productCode+"\') data-bs-toggle='modal' data-bs-target='#updateModal'>\n\
+        btnEdit = "<button onclick=loadData(\'" + it_product.productCode + "\') data-bs-toggle='modal' data-bs-target='#updateModal'>\n\
                    <i class='bi bi-pencil'></i>\n\
                    </button>";
-        
-        
+
+
         let productHTML = "<tr>\n\
-                        <td>"+ it_product.productCode +"</td>\n\
-                        <td>"+ it_product.name +"</td>\n\
-                        <td>"+ it_product.price +"</td>\n\
-                        <td>"+ it_product.units +"</td>\n\
-                        <td>"+ it_product.supplier +"</td>\n\
-                        <td>"+ btnDelete +"</td>\n\
-                        <td>"+ btnEdit +"</td>\n\
+                        <td>"+ it_product.productCode + "</td>\n\
+                        <td>"+ it_product.name + "</td>\n\
+                        <td>"+ it_product.price + "</td>\n\
+                        <td>"+ it_product.units + "</td>\n\
+                        <td>"+ it_product.supplier + "</td>\n\
+                        <td>"+ btnDelete + "</td>\n\
+                        <td>"+ btnEdit + "</td>\n\
                         </tr>";
-        listHTML += productHTML;     
+        listHTML += productHTML;
     }
-    
+
     document.querySelector('#dataTable tbody').outerHTML = listHTML;
-    
+
 }
 
-async function deleteProduct(productCode){
-    
+async function deleteProduct(productCode) {
+
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -55,7 +55,7 @@ async function deleteProduct(productCode){
         confirmButtonColor: '#43546F',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!'
-      }).then(async (result) => {
+    }).then(async (result) => {
         if (result.isConfirmed) {
             const request = await fetch('api/product/' + productCode, {
                 method: 'DELETE',
@@ -70,18 +70,18 @@ async function deleteProduct(productCode){
                 'The product has been deleted.',
                 'success'
             )
-            setTimeout(function(){
+            setTimeout(function () {
                 location.reload();
             }, 2000);
         }
-      })
+    })
 }
 
 
 
-async function loadData(productCode){
+async function loadData(productCode) {
     productToModify = productCode;
-    
+
     const request = await fetch('api/product/' + productCode, {
         method: 'GET',
         headers: {
@@ -90,17 +90,20 @@ async function loadData(productCode){
             'Authorization': localStorage.token
         }
     });
-    
+
     const productHTML = await request.json();
     console.log(productHTML.name);
-    
-    document.modalForm.inputPrice.value = productHTML.price;
-    document.modalForm.inputUnits.value = productHTML.units;
-    document.modalForm.inputSupplier.value = productHTML.supplier;
+
+    // document.modalForm.inputPrice.value = productHTML.price;
+    // document.modalForm.inputUnits.value = productHTML.units;
+    // document.modalForm.inputSupplier.value = productHTML.supplier;
+    document.getElementById("inputPrice").value = productHTML.price;
+    document.getElementById("inputUnits").value = productHTML.units;
+    document.getElementById("inputSupplier").value = productHTML.supplier;
     document.getElementById('productNameSpan').outerHTML = productHTML.name;
 }
 
-async function  modifyProduct(){
+async function modifyProduct() {
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -108,28 +111,28 @@ async function  modifyProduct(){
         timer: 3000,
         timerProgressBar: true,
         didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
     })
-      
+
     let data = {};
-    
+
     data.price = document.getElementById("inputPrice").value;
     data.units = document.getElementById("inputUnits").value;
     data.supplier = document.getElementById("inputSupplier").value;
-    
-    
+
+
     Swal.fire({
         title: 'Do you want to save the changes?',
         showDenyButton: true,
         showCancelButton: false,
         confirmButtonText: 'Save',
         denyButtonText: `Don't save`,
-      }).then(async (result) => {
+    }).then(async (result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-            const request = await fetch('/api/product/' + productToModify,{
+            const request = await fetch('/api/product/' + productToModify, {
                 method: 'PATCH',
                 headers: {
                     'Accept': 'application/json',
@@ -141,12 +144,12 @@ async function  modifyProduct(){
                     supplier: data.supplier
                 })
             });
-          
+
             Toast.fire({
                 icon: 'success',
                 title: 'Saved successfully'
             })
-            setTimeout(function(){
+            setTimeout(function () {
                 location.reload();
             }, 3000);
         } else if (result.isDenied) {
@@ -154,9 +157,50 @@ async function  modifyProduct(){
                 icon: 'warning',
                 title: 'Not Saved'
             })
-            setTimeout(function(){
+            setTimeout(function () {
                 location.reload();
             }, 3000);
         }
-      })
+    })
+}
+
+async function insertProduct() {
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    let data = {};
+
+    data.productCode = document.getElementById("input_product_code").value;
+    data.name = document.getElementById("input_name").value;
+    data.price = document.getElementById("input_price").value;
+    data.units = document.getElementById("input_units").value;
+    data.supplier = document.getElementById("input_supplier").value;
+
+    const request = await fetch('api/products', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) //La función agarra un objeto de js y lo transforma a JSON
+    });
+
+    Toast.fire({
+        icon: 'success',
+        title: 'Product Inserted successfully'
+    })
+    setTimeout(function () {
+        location.reload();
+    }, 3000);
+
 }
