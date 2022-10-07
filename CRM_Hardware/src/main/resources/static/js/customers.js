73e9c94fcef0bb4c35fc6c84fd7598bef7a66e8e
Julian362 <Juliangarcia362@hotmail.com>
2022-10-06 21:38:32 -0500

    Add loadCustomers method
    Add load method that brings the data to display in the table

--- CRM_Hardware/src/main/resources/static/js/customers.js
+++ CRM_Hardware/src/main/resources/static/js/customers.js
@@ -7,3 +7,51 @@ window.addEventListener("load", () =>{
     })
 })
 
+$(document).ready(function () {
+    loadCustomers();
+});
+
+
+
+async function loadCustomers() {
+
+    const request = await fetch('api/customer', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    const customersHTML = await request.json();
+    console.log(customersHTML);
+
+    let listHTML = '';
+
+    document.querySelector('#sanNumberUser1').outerHTML = customersHTML.length;
+    document.querySelector('#sanNumberUser2').outerHTML = customersHTML.length;
+    alert(customersHTML.length);
+
+    for (let it_customer of customersHTML) {
+        btnDelete = "<button class='btn-icons' onclick=deleteCustomer(\'" + it_customer.id + "\')>\n\
+                    <i class='bi bi-trash'></i>\n\
+                    </button>";
+        btnEdit = "<button class='btn-icons' onclick=loadDataCustomer(\'" + it_customer.id + "\') data-bs-toggle='modal' data-bs-target='#updateModal'>\n\
+                    <i class='bi bi-pencil'></i>\n\
+                    </button>";
+
+
+        let productHTML = "<tr>\n\
+                        <td>"+ it_customer.id + "</td>\n\
+                        <td>"+ it_customer.name + "</td>\n\
+                        <td>"+ it_customer.email + "</td>\n\
+                        <td>"+ it_customer.phone + "</td>\n\
+                        <td>"+ btnDelete + "</td>\n\
+                        <td>"+ btnEdit + "</td>\n\
+                        </tr>";
+        listHTML += productHTML;
+    }
+
+    document.querySelector('#tableCustomers tbody').outerHTML = listHTML;
+
+}
+
